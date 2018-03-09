bec20566e3f60ff92a229fcf0cdf3b75b088b99f
Paul <paul.du56@gmail.com>
2018-03-09 14:00:57 -0500

    +distance iteam are considered close to each other +distance at which a pigeon is scared from

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 <h2>Devoir 2 de Programmation Objet Avancée - Hiver 2018</h2>
 <b>Paul Michaud et Loïc Bothorel</b>
+
+Lorsqu'une nourriture est plus proche qu'un chien le pigeon ira quand même la manger (C'est un choix)
--- src/pigeonsquare/utils/Constants.java
+++ src/pigeonsquare/utils/Constants.java
@@ -3,5 +3,7 @@ package pigeonsquare.utils;
 public class Constants {
     public static final int SCREEN_HEIGHT = 800;
     public static final int SCREEN_WIDTH = 800;
+    public static final int IS_CLOSE = 15;
+    public static final int IS_SCARED = 400;
 
 }
