a675749f4d729f4b3a1fe5929f68fe5d501315c9
LBotho <loic.bothorel@icloud.com>
2018-03-10 16:43:56 -0500

    Constants

--- src/pigeonsquare/Food.java
+++ src/pigeonsquare/Food.java
@@ -1,10 +1,11 @@
 package pigeonsquare;
 
+import pigeonsquare.utils.Constants;
 import pigeonsquare.utils.Position;
 
 public class Food extends Item {
-    private static final String goodSeed = "assets/seed.png";
-    private static final String badSeed = "assets/bad_seed.png";
+    private static final String goodSeed = Constants.PATH_GOOD_SEED;
+    private static final String badSeed = Constants.PATH_BAD_SEED;
 
     private boolean isFresh;
     Food(Position position) {
@@ -16,10 +17,10 @@ public class Food extends Item {
     @Override
     public void run() {
         try {
-            Thread.sleep(2000);
+            Thread.sleep(Constants.FRESH_TIME);
             this.isFresh = false;
             this.changeImage(badSeed);
-            Thread.sleep(2000);
+            Thread.sleep(Constants.STALE_TIME);
             Environment.getInstance().removeItem(this);
         } catch (InterruptedException e) {
             e.printStackTrace();
--- src/pigeonsquare/utils/Constants.java
+++ src/pigeonsquare/utils/Constants.java
@@ -1,13 +1,35 @@
 package pigeonsquare.utils;
 
+/**
+ * Contains all the constants
+ */
 public class Constants {
-    public static final int MAX_PIGEON = 10;
-    public static final int MAX_FOOD = 5;
+    /**
+     * WINDOW
+     */
     public static final int SCREEN_HEIGHT = 800;
     public static final int SCREEN_WIDTH = 800;
-    public static final int IS_CLOSE = 15;
-    public static final int IS_SCARED = 400;
+
+    /**
+     * PIGEON
+     */
+    public static final int MAX_PIGEON = 10;
     public static final String PATH_BISET = "assets/biset.png";
     public static final String PATH_COLOMBIN = "assets/colombin.png";
     public static final String PATH_RAMIER = "assets/ramier.png";
+    public static final int IS_SCARED = 400;
+
+    /**
+     * FOOD
+     */
+    public static final int MAX_FOOD = 5;
+    public static final int FRESH_TIME = 3000;
+    public static final int STALE_TIME = 3000;
+    public static final String PATH_GOOD_SEED = "assets/seed.png";
+    public static final String PATH_BAD_SEED = "assets/bad_seed.png";
+
+    /**
+     * OTHERS
+     */
+    public static final int IS_CLOSE = 15;
 }
